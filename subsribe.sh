2d18907f02265cfc757bd1aac651dbd15c96415f
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-01-12 17:26:59 -0500

    Add batch to subscription script.

--- subsribe.sh
+++ subsribe.sh
@@ -3,7 +3,7 @@
 # subscribe lambda to cloudwatch and all lambdas
 exporters/cwl_to_elk/add_log_group.sh '/aws/cloudtrail/audit-and-data-access'
 
-for group in `aws logs describe-log-groups | jq -r '.logGroups[] | .logGroupName' | egrep '^/(gcp|aws/lambda/)'` ; do
+for group in `aws logs describe-log-groups | jq -r '.logGroups[] | .logGroupName' | egrep '^/(gcp|aws/lambda/|aws/batch/)'` ; do
   echo "Subscribing to: $group"
   exporters/cwl_to_elk/add_log_group.sh $group || echo "Subscription failed: $group"
 done
