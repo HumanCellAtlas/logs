5852161746b9a000ca030f815d70f22975dc1ee0
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-03-23 18:46:41 -0700

    Fix apps path in subscribe

--- scripts/subsribe.sh
+++ scripts/subsribe.sh
@@ -7,9 +7,9 @@ echo "$firehose_delivery_stream_arn"
 echo "$cwl_to_kinesis_role_arn"
 
 # subscribe lambda to cloudwatch and all lambdas
-processors/firehose_to_es_processor/add_log_group.sh $CLOUDTRAIL_LOG_GROUP_NAME $firehose_delivery_stream_arn $cwl_to_kinesis_role_arn
+apps/firehose_to_es_processor/add_log_group.sh $CLOUDTRAIL_LOG_GROUP_NAME $firehose_delivery_stream_arn $cwl_to_kinesis_role_arn
 
 for group in `aws logs describe-log-groups | jq -r '.logGroups[] | .logGroupName'` ; do
   echo "Subscribing to: $group"
-  processors/firehose_to_es_processor/add_log_group.sh $group $firehose_delivery_stream_arn $cwl_to_kinesis_role_arn || echo "Subscription failed: $group"
+  apps/firehose_to_es_processor/add_log_group.sh $group $firehose_delivery_stream_arn $cwl_to_kinesis_role_arn || echo "Subscription failed: $group"
 done
