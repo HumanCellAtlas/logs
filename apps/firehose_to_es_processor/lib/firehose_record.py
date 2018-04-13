41dc407a15f74e9b6d595388313b10672e63df9f
Parth Shah <parths@chanzuckerberg.com>
2018-04-12 21:57:54 -0700

    initial airbrake commit

--- apps/firehose_to_es_processor/lib/firehose_record.py
+++ apps/firehose_to_es_processor/lib/firehose_record.py
@@ -4,6 +4,12 @@ import json
 import base64
 from datetime import datetime
 from lib.util import extract_json
+import os
+from airbrake.notifier import Airbrake
+import re
+
+if os.environ.get('AIRBRAKE_FLAG'):
+    airbrake_notifier = Airbrake(project_id=os.environ.get("AIRBRAKE_PROJECT_ID"), api_key=os.environ.get("AIRBRAKE_API_KEY"))
 
 
 class FirehoseRecord():
@@ -40,4 +46,30 @@ class FirehoseRecord():
             for k, v in transformed_message.items():
                 transformed_payload[k] = json.dumps(v)
 
+        message = transformed_payload['@message']
+        log_group = transformed_payload['@log_group']
+        airbrake_flag = os.environ.get('AIRBRAKE_FLAG')
+        log_stream = self.record["logStream"]
+        if airbrake_flag and airbrake_flag == "True" and self._is_message_appropriate_for_airbrake(message, log_group):
+            airbrake_error = "'{0} {1} '@log_stream': {2}".format(log_group, message, log_stream)
+            try:
+                airbrake_notifier.notify(str(airbrake_error))
+            except:
+                pass
+
         return transformed_payload
+
+    def _is_message_appropriate_for_airbrake(self, message, log_group):
+        send_to_airbrake = False
+
+        blacklisted_log_group_names = os.environ.get("AIRBRAKE_BLACKLISTED_LOG_GROUP_NAMES")
+        blacklisted_log_group_names_regex_string = "|".join(blacklisted_log_group_names.split())
+        blacklisted_log_group_names_regexp = re.compile(blacklisted_log_group_names_regex_string, re.IGNORECASE)
+
+        whitelisted_log_message_terms = os.environ.get("AIRBRAKE_WHITELISTED_LOG_MESSAGE_TERMS")
+        whitelisted_log_message_terms_regex_string = "|".join(whitelisted_log_message_terms.split())
+        whitelisted_log_message_terms_regexp = re.compile(whitelisted_log_message_terms_regex_string, re.IGNORECASE)
+
+        if whitelisted_log_message_terms_regexp.search(message) and not blacklisted_log_group_names_regexp.search(log_group):
+            send_to_airbrake = True
+        return send_to_airbrake
