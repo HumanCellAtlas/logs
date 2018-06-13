3c69f1e75bd38605339387eb98f973acb76824f2
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-06-12 17:02:52 -0700

    Refactor the build target in cwl to slack app

--- apps/cwl_to_slack/main.tf
+++ apps/cwl_to_slack/main.tf
@@ -21,8 +21,6 @@ terraform {
 }
 
 
-variable "target_zip_path" {}
-
 resource "aws_iam_role" "slack_notifier" {
   name = "CloudWatchSlackNotifier"
   path = "/service-role/"
@@ -73,14 +71,21 @@ resource "aws_iam_role_policy" "slack_notifier_logs" {
 EOF
 }
 
+data "archive_file" "lambda_zip" {
+  type = "zip"
+  source_dir = "./target"
+  output_path = "./lambda.zip"
+}
+
 resource "aws_lambda_function" "slack_notifier" {
   function_name = "${var.app_name}"
-  filename = "${var.target_zip_path}"
+  filename = "${data.archive_file.lambda_zip.output_path}"
   description = "An Amazon SNS trigger that sends CloudWatch alarm notifications to Slack."
   runtime = "nodejs6.10"
   handler = "index.handler"
   memory_size = 128
   role = "${aws_iam_role.slack_notifier.arn}"
+  source_code_hash = "${base64sha256(file("${data.archive_file.lambda_zip.output_path}"))}"
   depends_on = [
     "aws_iam_role.slack_notifier",
     "aws_sns_topic.alarms"
