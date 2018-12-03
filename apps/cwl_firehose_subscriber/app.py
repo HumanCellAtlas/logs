32ff884ec22889afb71b7c8e02a7ad5a79722521
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-12-03 10:09:25 -0800

    Add instrumentation to catch issue #60

--- apps/cwl_firehose_subscriber/app.py
+++ apps/cwl_firehose_subscriber/app.py
@@ -42,12 +42,16 @@ def put_subscription_filter(log_group_name, destination_arn, role_arn):
 def handler(event, context):
     """Main Lambda function
     """
-    log_group_name = event['detail']['requestParameters']['logGroupName']
-    account_id = context.invoked_function_arn.split(":")[4]
-    delivery_stream_arn = "arn:aws:firehose:us-east-1:{0}:deliverystream/Kinesis-Firehose-ELK".format(account_id)
-    cwl_to_kinesis_role_arn = "arn:aws:iam::{0}:role/cwl-firehose".format(account_id)
-    if not blacklisted_log_groups.matches(log_group_name):
-        logger.info("Subscribing log group {0} to firehose".format(log_group_name))
-        put_subscription_filter(log_group_name, delivery_stream_arn, cwl_to_kinesis_role_arn)
-    else:
-        logger.info("Log group {0} is blacklisted. Not subscribed to firehose".format(log_group_name))
+    try:
+        log_group_name = event['detail']['requestParameters']['logGroupName']
+        account_id = context.invoked_function_arn.split(":")[4]
+        delivery_stream_arn = "arn:aws:firehose:us-east-1:{0}:deliverystream/Kinesis-Firehose-ELK".format(account_id)
+        cwl_to_kinesis_role_arn = "arn:aws:iam::{0}:role/cwl-firehose".format(account_id)
+        if not blacklisted_log_groups.matches(log_group_name):
+            logger.info("Subscribing log group {0} to firehose".format(log_group_name))
+            put_subscription_filter(log_group_name, delivery_stream_arn, cwl_to_kinesis_role_arn)
+        else:
+            logger.info("Log group {0} is blacklisted. Not subscribed to firehose".format(log_group_name))
+    except Exception as e:
+        logger.error(f"Failure for event: {event}")
+        raise e
